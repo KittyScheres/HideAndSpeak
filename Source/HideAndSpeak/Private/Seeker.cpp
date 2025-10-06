3a34518e6242b098be982da98cf000069fd9b031
Kitty <kittyhwscheres@gmail.com>
2025-10-06 12:57:47 +0200

    [ADDED] Added the first itteration of AI patroling behaviour

---
+++ Source/HideAndSpeak/Private/Seeker.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Seeker.h"
+
+// Sets default values
+ASeeker::ASeeker()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASeeker::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASeeker::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASeeker::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
